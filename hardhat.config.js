371e030c3dfcdfdb1565531e7713298cb7525c79
DESKTOP-CT815J6\pioneer <oliverchang3030@gmail.com>
2022-06-18 18:23:01 +0900

    add rinkeby & etherscan settings.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
+
+const ALCHEMY_API_KEY = "1ecae8a392624fd09342b2d2526d07a6";
+const RINKEBY_PRIVATE_KEY = "6a6791c11b545ee18a38140dec85fd47195b17e4c570942b2b4bd360339ab1d2";
 
 // If you are using MetaMask, be sure to change the chainId to 1337
 module.exports = {
@@ -11,6 +15,13 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 31337
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${ALCHEMY_API_KEY}`,
+      accounts: [`${RINKEBY_PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: '33Y5WCNTV2KVYK7VADA2PK4IMCRUG6ZY38'
   }
 };
